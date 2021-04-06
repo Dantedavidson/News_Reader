5b2487f934780051debbe74cdb8843d0c46537d4
Dantedavidson <50653094+Dantedavidson@users.noreply.github.com>
2021-04-06 15:39:32 +1000

    pagination working internally. Needs linking to api

--- src/components/common/PaginationBar.jsx
+++ src/components/common/PaginationBar.jsx
@@ -18,7 +18,12 @@ export const PaginationBar = ({ currentPage, total, perPage }) => {
   });
 
   const next = () => {
-    if (display.active === display.pages) {
+    if (display.active >= display.pages - (perPage - 1)) {
+      console.log("display last ten");
+      setDisplay((prevState) => ({
+        ...prevState,
+        active: prevState.active + 1,
+      }));
       return;
     }
     const active = display.active + 1;
@@ -33,7 +38,12 @@ export const PaginationBar = ({ currentPage, total, perPage }) => {
     }));
   };
   const back = () => {
-    if (display.active === 1) {
+    if (display.active > display.pages - (perPage - 1)) {
+      console.log(display.active, display.pages - (perPage - 1));
+      setDisplay((prevState) => ({
+        ...prevState,
+        active: prevState.active - 1,
+      }));
       return;
     }
     const active = display.active - 1;
@@ -61,8 +71,10 @@ export const PaginationBar = ({ currentPage, total, perPage }) => {
   };
 
   const last = () => {
-    const active = display.pages - perPage;
-    const last = active + perPage;
+    if (display.active === display.pages - perPage) return;
+    const active = display.pages - (perPage - 1);
+
+    const last = active - 1 + perPage;
     const first = last - perPage;
     setDisplay((prevState) => ({
       ...prevState,
@@ -74,15 +86,10 @@ export const PaginationBar = ({ currentPage, total, perPage }) => {
   };
 
   useEffect(() => {
-    for (let n = 1; n < display.pages; n++) {
+    for (let n = 1; n <= display.pages; n++) {
       setDisplay((prevState) => ({
         ...prevState,
-        items: [
-          ...prevState.items,
-          <Pagination.Item key={n} active={n === display.active}>
-            {n}
-          </Pagination.Item>,
-        ],
+        items: [...prevState.items, n],
       }));
     }
     setDisplay((prevState) => ({
@@ -94,16 +101,45 @@ export const PaginationBar = ({ currentPage, total, perPage }) => {
       ),
     }));
   }, []);
+  useEffect(() => {
+    console.log(display.first, display.last, display.active);
+  }, [display]);
 
   return (
     <div>
       {" "}
       <Pagination>
-        <Pagination.First onClick={first} />
-        <Pagination.Prev onClick={back} />
-        {display.display.map((item) => item)}
-        <Pagination.Next onClick={next} />
-        <Pagination.Last onClick={last} />
+        <Pagination.First
+          onClick={first}
+          className={display.active <= perPage ? "display-none" : ""}
+        />
+        <Pagination.Prev
+          onClick={back}
+          className={display.active === 1 ? "display-none" : ""}
+        />
+        {display.display.map((item) => (
+          <Pagination.Item
+            key={item}
+            active={item === display.active}
+            className={
+              item === display.active ? "page-item active" : "page-item"
+            }
+          >
+            {item}
+          </Pagination.Item>
+        ))}
+        <Pagination.Next
+          onClick={next}
+          className={display.active === display.pages ? "display-none" : ""}
+        />
+        <Pagination.Last
+          onClick={last}
+          className={
+            display.active >= display.pages - (perPage - 1)
+              ? "display-none"
+              : ""
+          }
+        />
       </Pagination>
     </div>
   );
